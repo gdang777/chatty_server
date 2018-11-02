fa10acbcd6720f3c8c1586bce7c1710e54024168
Gaurav <netgary7@gmail.com>
2018-11-02 00:39:04 +0000

    Cleaned code for final submit

--- server.js
+++ server.js
@@ -4,21 +4,15 @@ const SocketServer = require('ws').Server;
 const uuidv4 = require('uuid/v4');
 
 
-// Set the port to 3001
 const PORT = 3001;
 
-// Create a new express server
+//-------- new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-// Create the WebSockets server
-const wss = new SocketServer({ server });
+const wss = new SocketServer({server});
 
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
   let users = {
@@ -29,9 +23,8 @@ wss.on('connection', (ws) => {
   console.log("connections", users);
 
   wss.clients.forEach(client => {
-          client.send(JSON.stringify(users));
-      });
-
+      client.send(JSON.stringify(users));
+    });
 
   ws.on('message', (incomingMessageOnServer) => {
     const parsedMessage = JSON.parse(incomingMessageOnServer);
@@ -61,19 +54,15 @@ wss.on('connection', (ws) => {
       break;
     }
   })
-
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => {
     console.log('Client disconnected');
-   let users = {
+  let users = {
         id: uuidv4(),
         type: 'connectedUsers',
         count: wss.clients.size
       }
-  console.log("connections", users);
-
   wss.clients.forEach(client => {
-          client.send(JSON.stringify(users));
-      });
+      client.send(JSON.stringify(users));
+    });
   });
 });
